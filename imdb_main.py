49d270d037767ed012bac2785a26e4cfcc92f7ea
antoine_tixier <anti5662@lix.polytechnique.fr>
2017-04-04 12:05:05 +0200

    various improvements and fixes

--- imdb_main.py
+++ imdb_main.py
@@ -1,15 +1,13 @@
-# author: Antoine Tixier (see companion handout for a theoretical intro: http://www.lix.polytechnique.fr/~anti5662/intro_cnn_lstm_tixier.pdf)
-# gets to 0.895 accuracy on the test set usually within 2 to 4 epochs (2 mins per epoch on NVidia TITAN)
+# see companion handout for a theoretical intro: http://www.lix.polytechnique.fr/~anti5662/intro_cnn_lstm_tixier.pdf
+# gets to ~0.895 accuracy on the test set usually within 2 to 4 epochs (~160s per epoch on NVidia TITAN)
 # tested on ubuntu with Python 3, Keras version 1.1.0., tensorflow backend
 
 import csv
 import json
 import numpy as np
 
-from gensim import utils
 from gensim.models.word2vec import Word2Vec
 
-from keras.preprocessing import sequence
 from keras.models import Model
 from keras.callbacks import EarlyStopping
 from keras.constraints import maxnorm
@@ -18,23 +16,35 @@ from keras.layers import Convolution1D, GlobalMaxPooling1D, Dense, Embedding, In
 print('packages loaded')
 
 path_to_IMDB = ''
+use_pretrained = True
+do_early_stopping = True
+model_save = False
+if model_save:
+    name_save = 'imdb_cnn_pre_400_04_04_17'
+    print('model will be saved under name:',name_save)
 
-# ========== read pre-processed data ==========
+# ========== parameter values ==========
 
 max_features = int(2e4)
-stpwd_thd = 1 # TODO: allow greater values than 1 
+stpwd_thd = 10 # TODO: allow greater values than 1 - should be fixed now
 max_size = int(4e2)
 word_vector_dim = int(3e2)
 do_non_static = True
 nb_filters = 200
 drop_rate = 0.3
 batch_size = 32
-nb_epoch = 10
+nb_epoch =  10 # increasing the number of epochs may lead to overfitting when max_size is small (especially since dataset is small in the 1st place)
 my_optimizer = 'adam' # proved better than SGD and Adadelta
+my_patience = 2 # for early stopping strategy
+
+if not use_pretrained:
+    # if the embeddings are initialized randomly, using static mode doesn't make sense
+    do_non_static = True
+    print("not using pre-trained embeddings, overwriting 'do_non_static' argument")
 
 print('=== parameter values: ===')
 print('top',max_features,'words used as features')
-print('top',stpwd_thd-1,'words excluded')
+print('top',stpwd_thd,'words excluded')
 print('max size of doc (in words):',max_size)
 print('dim of word vectors:',word_vector_dim)
 print('non-static:',do_non_static)
@@ -43,6 +53,8 @@ print('dropout rate:',drop_rate)
 print('batch size:',batch_size)
 print('number of epochs:',nb_epoch)
 print('optimizer:',my_optimizer)
+print('patience:',my_patience)
+print('=== end parameter values ===')
 
 # ========== read pre-processed data ==========
 
@@ -79,9 +91,6 @@ print('data loaded')
 # only take into account the 'max_features' most frequent words
 # disregard the 'stopword_threhsold' most frequent words
 
-# to display the 'stopwords'
-#[index_to_word[idx] for idx in range(1,stpwd_thd)]
-
 x_train = [[elt for elt in rev if elt<=max_features and elt>=stpwd_thd] for rev in x_train]
 x_test =  [[elt for elt in rev if elt<=max_features and elt>=stpwd_thd] for rev in x_test]
 
@@ -93,8 +102,11 @@ print('pruning done')
 x_train = [rev[:max_size] for rev in x_train]
 x_test = [rev[:max_size] for rev in x_test]
 
-# pad reviews shorter than 'max_size'
+# pad reviews shorter than 'max_size' with zeroes
 # the vector of the 0th index will be set to all zeroes (zero padding strategy)
+
+print('padding',len([elt for elt in x_train if len(elt)<max_size])
+,'reviews from the training set')
 
 x_train = [rev+[0]*(max_size-len(rev)) if len(rev)<max_size else rev for rev in x_train]
 
@@ -103,6 +115,9 @@ if max_size == list(set([len(rev) for rev in x_train]))[0]:
     print('1st sanity check passed')
 else:
     print('1st sanity check failed !')
+
+print('padding',len([elt for elt in x_test if len(elt)<max_size])
+,'reviews from the test set')
 
 x_test = [rev+[0]*(max_size-len(rev)) if len(rev)<max_size else rev for rev in x_test]
 
@@ -118,62 +133,88 @@ print('truncation and padding done')
 # invert mapping
 index_to_word = dict((v,k) for k, v in word_to_index.items())
 
+# to display the 'stopwords'
+print('stopwords are:',[index_to_word[idx] for idx in range(1,stpwd_thd)])
+
 # convert integer reviews into word reviews
 x_full = x_train + x_test
-
 x_full_words = [[index_to_word[idx] for idx in rev if idx!=0] for rev in x_full]
-
-# initialize word vectors
-word_vectors = Word2Vec(size=word_vector_dim, min_count=1)
-
-# create entries for the words in our vocabulary
-word_vectors.build_vocab(x_full_words)
-
-# sanity check
 all_words = [word for rev in x_full_words for word in rev]
 
-if len(list(set(all_words))) == len(word_vectors.wv.vocab):
-    print('3rd sanity check passed')
+print(len(all_words),'words')
+print(len(list(set(all_words))),'unique words')
+
+if use_pretrained:
+
+    # initialize word vectors
+    word_vectors = Word2Vec(size=word_vector_dim, min_count=1)
+
+    # create entries for the words in our vocabulary
+    word_vectors.build_vocab(x_full_words)
+
+    # sanity check
+    if len(list(set(all_words))) == len(word_vectors.wv.vocab):
+        print('3rd sanity check passed')
+    else:
+        print('3rd sanity check failed !')
+
+    # fill entries with the pre-trained word vectors
+    path_to_pretrained_wv = ''
+    word_vectors.intersect_word2vec_format(path_to_pretrained_wv + 'GoogleNews-vectors-negative300.bin', binary=True)
+
+    print('pre-trained word vectors loaded')
+
+    # NOTE: in-vocab words without an entry in the binary file are not removed from the vocabulary
+    # instead, their vectors are silently initialized to random values
+
+    # if necessary, we can detect those vectors via their norms which approach zero
+    #norms = [np.linalg.norm(word_vectors[word]) for word in word_vectors.wv.vocab.keys()]
+    #idxs_zero_norms = [idx for idx, norm in enumerate(norms) if norm<0.05]
+    # most of those words are proper nouns, like patton, deneuve, etc.
+    # they don't have an entry in the word vectors because we lowercased the text
+    #no_entry_words = [word_vectors.wv.vocab.keys()[idx] for idx in idxs_zero_norms]
+
+    # create numpy array of embeddings
+    embeddings = np.zeros((max_features + 1,word_vector_dim))
+    for word in word_vectors.wv.vocab.keys():
+        idx = word_to_index[word]
+        # word_to_index is 1-based! the 0-th row, used for padding, stays at zero
+        embeddings[idx,] = word_vectors[word]
+
+    print('embeddings created')
+
 else:
-    print('3rd sanity check failed !')
-
-# fill entries with the pre-trained word vectors
-path_to_pretrained_wv = ''
-word_vectors.intersect_word2vec_format(path_to_pretrained_wv + 'GoogleNews-vectors-negative300.bin', binary=True)
-
-print('pre-trained word vectors loaded')
-
-# NOTE: in-vocab words without an entry in the binary file are not removed from the vocabulary
-# instead, their vectors are silently initialized to random values
-
-# if necessary, we can detect those vectors via their norms which approach zero
-#norms = [np.linalg.norm(word_vectors[word]) for word in word_vectors.wv.vocab.keys()]
-#idxs_zero_norms = [idx for idx, norm in enumerate(norms) if norm<0.05]
-# most of those words are proper nouns, like Patton, Deneuve, etc.
-# they don't have an entry in the word vectors because we lowercased the text
-#no_entry_words = [word_vectors.wv.vocab.keys()[idx] for idx in idxs_zero_norms]
-
-# create numpy array of embeddings
-embeddings = np.zeros((max_features + 1,word_vector_dim))
-for word in word_vectors.wv.vocab.keys():
-    idx = word_to_index[word]
-    # word_to_index is 1-based! the 0-th row, used for padding, stays at zero
-    embeddings[idx,] = word_vectors[word]
-
-print('embeddings created')
+    print('not using pre-trained embeddings')
 
 # ========== training CNN ==========
 
+#max([max(elt) for elt in x_full])
+
 my_input = Input(shape=(max_size,), dtype='int32') # for some reason here it is important to let the second argument of shape blank
 
-embedding = Embedding(input_dim = len(word_vectors.wv.vocab) + 1, # vocab size, including the 0-th word used for padding
-                      output_dim = word_vector_dim,
-                      #input_length = max_size, # length of input sequences
-                      dropout = drop_rate, # see http://www.cs.toronto.edu/~rsalakhu/papers/srivastava14a.pdf
-                      #embeddings_constraint = maxnorm(3.),
-                      weights=[embeddings], # we pass our pre-trained embeddings
-                      trainable = do_non_static # non-static mode
-                      ) (my_input)
+# NOTE: create embedding tables with dimensions based on max_features, ignoring stopword removal and truncation
+# for instance if initially max_features = 2e4, reviews will be composed of integers from 1 to 2e4
+# but after truncation and stopword removal, the actual length of the vocabulary (number of unique integer values) may be much smaller
+# but if input dim is based on this final voc size, some integers still present (in the range [1,2e4]) won't have a row anymore
+# so we create the embedding lookup table based on original max_features value, knowing that the words that have been removed just won't be looked up
+
+if use_pretrained:
+    embedding = Embedding(input_dim = max_features + 1, # vocab size, including the 0-th word used for padding
+                          output_dim = word_vector_dim,
+                          #input_length = max_size, # length of input sequences
+                          dropout = drop_rate, # see http://www.cs.toronto.edu/~rsalakhu/papers/srivastava14a.pdf
+                          #embeddings_constraint = maxnorm(3.),
+                          weights=[embeddings], # we pass our pre-trained embeddings
+                          trainable = do_non_static
+                          ) (my_input)
+else:
+    embedding = Embedding(input_dim = max_features + 1, # vocab size, including the 0-th word used for padding
+                          output_dim = word_vector_dim,
+                          #input_length = max_size, # length of input sequences
+                          dropout = drop_rate, # see http://www.cs.toronto.edu/~rsalakhu/papers/srivastava14a.pdf
+                          #embeddings_constraint = maxnorm(3.),
+                          trainable = do_non_static
+                          ) (my_input)
 
 # size of the feature map should be equal to max_size-filter_length+1
 conv_1 = Convolution1D(nb_filter = nb_filters,
@@ -215,22 +256,33 @@ model.compile(loss='binary_crossentropy',
 
 print('model compiled')
 
-early_stopping = EarlyStopping(monitor='val_acc', # we go through the epochs as long as the accuracy on the validation set increases
-                               patience = 3,
-                               mode = 'max')
-
-model.fit(x_train, 
-          y_train,
-          batch_size = batch_size,
-          nb_epoch = nb_epoch,
-          validation_data = (x_test, y_test),
-          callbacks = [early_stopping])
+early_stopping = EarlyStopping(monitor='val_loss', # go through epochs as long as loss on validation set decreases
+                               patience = my_patience,
+                               mode = 'min')
+if do_early_stopping:
+    print('using early stopping strategy')
+    
+    model.fit(x_train, 
+              y_train,
+              batch_size = batch_size,
+              nb_epoch = nb_epoch,
+              validation_data = (x_test, y_test),
+              callbacks = [early_stopping])
+else:
+    
+    model.fit(x_train, 
+              y_train,
+              batch_size = batch_size,
+              nb_epoch = nb_epoch,
+              validation_data = (x_test, y_test),
+              )
 
 # persist model to disk
-
-model.save(path_to_IMDB + 'imdb_cnn_3_30_17')
-
-print('model saved to disk')
+if model_save:
+    
+    model.save(path_to_IMDB + name_save)
+    
+    print('model saved to disk')
 
 #loss, acc = model.evaluate(x_test, y_test, batch_size = batch_size)
 
